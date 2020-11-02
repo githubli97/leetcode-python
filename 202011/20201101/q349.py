3434ceecde69048a5358df02eceafd1ef11c1374
lihang <lihang@kaoshixing.com>
2020-11-02 09:16:49 +0800

    改

--- 202011/20201101/q349.py
+++ 202011/20201101/q349.py
@@ -3,8 +3,4 @@ from typing import List
 
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        result = []
-        for i in nums1:
-            if i in nums2 and i not in result:
-                result.append(i)
-        return result
+        return set(nums1) & set(nums2)
